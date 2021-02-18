d79269c4b180891415103592c869f425698506a2
Anupmor1998 <53808559+Anupmor1998@users.noreply.github.com>
2021-02-18 15:33:50 +0530

    Final commit

--- src/components/TempApp.js
+++ src/components/TempApp.js
@@ -1,44 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Temp.css";
-function TempApp(props) {
-  const { city, search, showWeather, cod, cityName } = props;
+
+function TempApp() {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("");
+
+  const fetchApi = async () => {
+    const apiKey = "3c9ab84472c05b6f1dcca909970fe924";
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${apiKey}`;
+    // const url = `https://api.openweathermap.org/data/2.5/forecast?q=${search}&units=metric&appid=${apiKey}`;
+
+    const response = await fetch(url);
+    const resJSON = await response.json();
+    setCity(resJSON);
+  };
+
+  const showWeather = (event) => {
+    setSearch(event.target.value);
+  };
   return (
     <>
       <div className="box">
+        <div className="header">Weather App</div>
+
         <div>
+          <h4>Enter the name of the city to get started!</h4>
           <input
             className="inputData"
             type="text"
             onChange={showWeather}
             value={search}
           />
+          <button className="btn" onClick={fetchApi}>
+            <i class="fas fa-arrow-right"></i>
+          </button>
         </div>
 
-        {/* <button className="btn" onClick={fetchApi}>
-          Get Weather
-        </button> */}
-        {console.log(city)}
-
-        {city && cod == "200" ? (
+        {city && city.cod == "200" ? (
           <div>
-            <h2>
-              <i className="fas fa-street-view"></i>&nbsp;&nbsp;
-              {cityName}
-            </h2>
+            <p className="para1">Weather Forecast of {city.name}</p>
+
             <div>
               <img
                 src={`http://openweathermap.org/img/w/${city.weather[0].icon}.png`}
                 alt="wthr img"
               />
             </div>
-
-            <h1>{city.main.temp} Ce</h1>
-            <h3>
-              Min: {city.main.temp_min} Ce | Max: {city.main.temp_max} Ce
-            </h3>
+            <div className="mid-section">
+              <p className="para2">Temperture</p>
+              <h1>{city.main.temp} °C</h1>
+            </div>
+            <div className="mid-section">
+              <p className="para2">Min Temperture</p>
+              <h1>{city.main.temp} °C</h1>
+            </div>
+            <div className="mid-section">
+              <p className="para2">Max Temperture</p>
+              <h1>{city.main.temp} °C</h1>
+            </div>
           </div>
         ) : (
-          <p className="error-msg">No Data Found!</p>
+          ""
         )}
       </div>
     </>
